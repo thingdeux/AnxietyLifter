fba6105f4c253c264e1a1726aef5725491a04481
Joshua Johnson <me@josh.land>
2021-08-08 08:57:13 -0700

    Initial Commit

---
+++ AnxietyLifter/AnxietyLifterApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnxietyLifterApp.swift
+//  AnxietyLifter
+//
+//  Created by Joshua Danger on 8/8/21.
+//
+
+import SwiftUI
+
+@main
+struct AnxietyLifterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
